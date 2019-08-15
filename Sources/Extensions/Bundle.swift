b2c81cad51fe3d2bc9bb8928f0b05f8f59276282
Su Ho <su@nimblehq.co>
2019-08-15 10:11:12 +0700

    Make public

--- Sources/Extensions/Bundle.swift
+++ Sources/Extensions/Bundle.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 extension Bundle {
-    var shortVersion: String? {
+    public var shortVersion: String? {
         return object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
     }
 
-    var buildVersion: String? {
+    public var buildVersion: String? {
         return object(forInfoDictionaryKey: "CFBundleVersion") as? String
     }
 }
