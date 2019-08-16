d244e4f625184599294cd76b774dc414a3c04538
Su Ho <su@nimblehq.co>
2019-08-16 18:26:41 +0700

    Update UITableView.swift

--- Sources/Extensions/UITableView.swift
+++ Sources/Extensions/UITableView.swift
@@ -1,21 +1,21 @@
 extension UITableView {
-    func dequeueReusableCell<T: UITableViewCell>(withClass name: T.Type) -> T {
+    public func dequeue<T: UITableViewCell>(_ name: T.Type) -> T {
         return dequeueReusableCell(withIdentifier: String(describing: name)) as! T
     }
 
-    func dequeueReusableCell<T: UITableViewCell>(withClass name: T.Type, for indexPath: IndexPath) -> T {
+    public func dequeue<T: UITableViewCell>(_ name: T.Type, for indexPath: IndexPath) -> T {
         return dequeueReusableCell(withIdentifier: String(describing: name), for: indexPath) as! T
     }
 
-    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(withClass name: T.Type) -> T {
+    public func dequeue<T: UITableViewHeaderFooterView>(_ name: T.Type) -> T {
         return dequeueReusableHeaderFooterView(withIdentifier: String(describing: name)) as! T
     }
 
-    func register<T: UITableViewHeaderFooterView>(headerFooterViewClassWith name: T.Type) {
+    public func register<T: UITableViewHeaderFooterView>(_ name: T.Type) {
         register(T.self, forHeaderFooterViewReuseIdentifier: String(describing: name))
     }
 
-    func register<T: UITableViewCell>(cellWithClass name: T.Type) {
+    public func register<T: UITableViewCell>(_ name: T.Type) {
         register(T.self, forCellReuseIdentifier: String(describing: name))
     }
 }
