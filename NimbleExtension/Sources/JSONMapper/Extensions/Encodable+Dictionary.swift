43469f573456fb6e0f316edfcac12641dda7f5fa
Tam Nguyen <tam.v@nimblehq.co>
2020-11-17 15:37:02 +0700

    Add extension

---
+++ NimbleExtension/Sources/JSONMapper/Extensions/Encodable+Dictionary.swift
@@ -0,0 +1,17 @@
+//
+//  Encodable+Dictionary.swift
+//  NimbleExtension
+//
+//  Created by Tam Nguyen on 11/17/20.
+//  Copyright © 2020 Nimble. All rights reserved.
+//
+
+import Foundation
+
+public extension Encodable {
+
+    var dictionary: [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
+    }
+}
