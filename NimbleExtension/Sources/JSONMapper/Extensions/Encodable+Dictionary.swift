4b49adf0ce4c6298914dc12c2d53b864e7b43492
Tam Nguyen <tam.v@nimblehq.co>
2020-11-18 16:27:53 +0700

    Update dictionary to toDictionary

--- NimbleExtension/Sources/JSONMapper/Extensions/Encodable+Dictionary.swift
+++ NimbleExtension/Sources/JSONMapper/Extensions/Encodable+Dictionary.swift
@@ -10,10 +10,12 @@ import Foundation
 
 public extension Encodable {
 
-    var dictionary: [String: Any]? {
-        guard let data = try? JSONEncoder().encode(self) else { return nil }
-        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap {
-            $0 as? [String: Any]
+    func toDictionary(_ encoder: JSONEncoder = JSONEncoder()) -> [String: Any] {
+        guard let dictionary = try? JSONSerialization.jsonObject(
+            with: encoder.encode(self), options: .allowFragments
+        ) as? [String: Any] else {
+            return [:]
         }
+        return dictionary
     }
 }
